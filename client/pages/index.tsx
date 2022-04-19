75f24f1af7cd5995e284c63d2103eae952dda885
Andreas Olsson <olsson.andreas@gmail.com>
2022-04-19 13:07:04 +0200

    Show submit button on search

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -19,8 +19,14 @@ const Home: NextPage = () => {
     return (
         <>
             <form className="w-full" onSubmit={handleSubmit}>
-                <input title="Only a single word" pattern="[a-zA-ZÅÄÖåäö]*" required className="bg-[#2F3E46] text-[#CAD2C5] shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="word" type="text" placeholder="Search synonyms for..." />
-                <input type="submit" className="hidden" value="Search" />
+                <div className="flex items-end mb-3">
+                    <div className="relative mr-3 w-full">
+                        <input title="Only a single word" pattern="[a-zA-ZÅÄÖåäö]*" required className="bg-[#2F3E46] text-[#CAD2C5] shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="word" type="text" placeholder="Search synonyms for..." />
+                    </div>
+                    <div>
+                        <input className="border rounded py-2 px-3 mb-3 leading-tight hover:text-[#84A9BC]" type="submit" value="Search" />
+                    </div>
+                </div>
             </form>
         </>
     )
